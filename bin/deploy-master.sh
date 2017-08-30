4af934a2f737f071c4e3a6f8a9103af74cfc66c8
James Loosli <james.loosli@hart.com>
2017-08-30 11:25:45 -0700

    Revert "Puppetfile, Vagrantfile, and a master deploy attempt for the vagrant setup"
    This reverts commit 1fe13800225074fd7cb9b742c49567c2e6a68485.

--- bin/deploy-master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo rpm -Uvh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
-sudo yum install -y \
-  puppetserver
-
-#sudo /opt/puppetlabs/puppet/bin/gem install r10k
-
