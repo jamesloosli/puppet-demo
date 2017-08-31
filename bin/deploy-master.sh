354ad96a11939b451a38e3c16186b9d7fcf6f085
James Loosli <loosli@gmail.com>
2017-08-30 17:07:56 -0700

    The one where I add one little flag and it all works

--- bin/deploy-master.sh
+++ bin/deploy-master.sh
@@ -16,4 +16,4 @@ sudo cp /tmp/r10k.yaml /etc/puppetlabs/r10k/r10k.yaml
 
 sudo echo "github.com,192.30.255.113 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > /root/.ssh/known_hosts
 
-sudo /opt/puppetlabs/puppet/bin/r10k deploy environment
+sudo /opt/puppetlabs/puppet/bin/r10k deploy environment -p
